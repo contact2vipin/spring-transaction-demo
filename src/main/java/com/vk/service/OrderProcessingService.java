c2f9c49d86f1cfe894c6c988f8b8f005b108e9f5
VIPIN KUMAR <contact2vipink@gmail.com>
2026-09-14 02:31:13 +0530

    1. Added Propagation.REQUIRED

--- src/main/java/com/vk/service/OrderProcessingService.java
+++ src/main/java/com/vk/service/OrderProcessingService.java
@@ -5,8 +5,7 @@ import com.vk.entities.Product;
 import com.vk.records.request.OrderRequest;
 import com.vk.records.response.OrderResponse;
 import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Isolation;
-//import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
@@ -22,8 +21,8 @@ public class OrderProcessingService {
         this.inventoryService = inventoryService;
     }
 
-//    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED)
-    @Transactional
+    // REQUIRED: join an existing transaction or create a new one if not exist
+    @Transactional(propagation = Propagation.REQUIRED)
     public OrderResponse placeAnOrder(OrderRequest orderRequest) {
         // get Product inventory
         Product product = inventoryService.getProduct(orderRequest.productId());
--- src/main/java/com/vk/service/OrderService.java
+++ src/main/java/com/vk/service/OrderService.java
@@ -3,6 +3,8 @@ package com.vk.service;
 import com.vk.entities.Order;
 import com.vk.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderService {
@@ -13,6 +15,7 @@ public class OrderService {
         this.orderRepository = orderRepository;
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     public Order saveOrder(Order order) {
         return orderRepository.save(order);
     }
